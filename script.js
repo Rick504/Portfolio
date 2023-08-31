308921dff768409971532391ba6eb7c72dc1f971
Rick Wesley <rick.wes1@gmail.com>
2023-08-31 19:39:46 -0300

    feat: first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+const textArray = ['hard', 'fun', 'a journey', 'LIFE'];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains('typing'))
+      cursorSpan.classList.add('typing');
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove('typing');
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains('typing'))
+      cursorSpan.classList.add('typing');
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove('typing');
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+function showElement(id, classOne, classTwo) {
+  document.querySelector(id).classList.add(classOne);
+  document.querySelector(id).classList.remove(classTwo);
+}
+
+function hideElement(id, classOne, classTwo) {
+  document.querySelector(id).classList.add(classOne);
+  document.querySelector(id).classList.remove(classTwo);
+}
+
+window.addEventListener('scroll', function () {
+  var scrollPosition = window.scrollY;
+  // console.log(scrollPosition);
+  if (scrollPosition > 200) {
+    showElement('#about', 'effect-pull-show', 'effect-pull-hide');
+  } else {
+    hideElement('#about', 'effect-pull-hide', 'effect-pull-show');
+  }
+});
